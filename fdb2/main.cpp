5fd571beb41ed505467933d037f7b3547f507317
feldsherov <feldsherov_sv@mail.ru>
2015-04-12 12:58:00 +0300

    Initial Commit

---
+++ fdb2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  fdb2
+//
+//  Created by Святослав Фельдшеров on 12.04.15.
+//  Copyright (c) 2015 Святослав Фельдшеров. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
